73ece6eceb08bf2671fca9a858c6c578a18206ee
reginaldo <reginaldolribeiro@gmail.com>
2019-02-18 17:44:18 -0300

    Views

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-  return res.send('Hello GoBarber!')
+  // return res.send('Hello GoBarber!')
+  return res.render('auth/signup.njk')
 })
 
 module.exports = routes
